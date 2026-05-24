f4ae9485bf13d5d494dd436f970676b46e14a5bd
S3NJU <s3nju.best.ofc@gmail.com>
2026-05-24 01:34:19 -0300

    Create pinterest.js

---
+++ commands/search/pinterest.js
@@ -0,0 +1,84 @@
+import fetch from 'node-fetch'
+
+export default {
+  command: ['pinterest', 'pin'],
+  category: 'buscador',
+  run: async (client, m, args, from) => {
+    const text = args.join(' ')
+    const isPinterestUrl = /^https?:\/\//.test(text)
+
+    if (!text) {
+      return client.reply(
+        m.chat,
+        `🍒 Ingresa un *término* de búsqueda o un enlace de *Pinterest*.`,
+        m,
+        global.rcanal
+      )
+    }
+
+    try {
+      if (isPinterestUrl) {
+        const pinterestUrl = `${api.url}/dl/pinterest?url=${text}&key=${api.key}`
+        const ress = await fetch(pinterestUrl)
+        if (!ress.ok) throw new Error(`La API devolvió un código de error: ${ress.status}`)
+
+        const { data: result } = await ress.json()
+        const mediaType = ['image', 'video'].includes(result.type) ? result.type : 'document'
+        const isVideo = result.type === 'video'
+
+        const message2 =
+          `🍁 ꨩᰰ𑪐𑂺 ˳ ׄ 𝖯𝗂𝗇𝗍𝖾𝗋𝖾𝗌𝗍 𝖣𝗈𝗐𝗇𝗅𝗈𝖺𝖽𝖾𝗋  ࣭𑁯ᰍ   ̊ ܃܃\n\n` +
+          `> 🍃 ᩴᩴ̴𖫲᮫ִ۫𝆬  Resultados para tu enlace › *${text}*\n\n` +
+          `𖣣ֶㅤ֯⌗ 🍄̷ ׄ ⬭ Título › *${result.title}*\n` +
+          `𖣣ֶㅤ֯⌗ 🍄̷ ׄ ⬭ Tipo › *${isVideo ? "Video" : "Imagen"}*`; 
+
+        await client.sendMessage(
+          m.chat,
+          { [mediaType]: { url: result.dl }, caption: message2 },
+          { quoted: m, ...global.rcanal },
+        )
+      } else {
+        const pinterestAPI = `${api.url}/search/pinterest?query=${text}&key=${api.key}`
+        const res = await fetch(pinterestAPI)
+        if (!res.ok) throw new Error(`La API devolvió un código de error: ${res.status}`)
+
+        const jsons = await res.json()
+        const json = jsons.data
+
+        if (!json || json.length === 0) {
+          return client.reply(
+            m.chat, 
+            `✐ No se encontraron resultados para *${text}*`, 
+            m, 
+            global.rcanal
+          )
+        }
+
+        const index = Math.floor(Math.random() * json.length)
+        const result = json[index]
+
+        const message =
+          `🍁 ꨩᰰ𑪐𑂺 ˳ ׄ 𝖯𝗂𝗇𝗍𝖾𝗋𝖾𝗌𝗍 𝖲𝖾𝖺𝗋𝖼𝗁 ࣭𑁯ᰍ   ̊ ܃܃\n\n` +
+          `> 🍃 ᩴᩴ̴𖫲᮫ִ۫𝆬   Resultados para › *${text}*\n\n` +
+          `𖣣ֶㅤ֯⌗ 🍄̷ ׄ ⬭  Título › *${result.title}*\n` +
+          `𖣣ֶㅤ֯⌗ 🍄̷ ׄ ⬭  Descripción › *${result.description}*\n` +
+          `𖣣ֶㅤ֯⌗ 🍄̷ ׄ ⬭  Autor › *${result.full_name}*\n` +
+          `𖣣ֶㅤ֯⌗ 🍄̷ ׄ ⬭  Likes › *${result.likes}*\n` +
+          `𖣣ֶㅤ֯⌗ 🍄̷ ׄ ⬭  Publicado › *${result.created}*`;
+
+        await client.sendMessage(
+          m.chat,
+          { image: { url: result.hd }, caption: message },
+          { quoted: m, ...global.rcanal },
+        )
+      }
+    } catch (e) {
+      await client.reply(
+        m.chat,
+        msgglobal + e,
+        m,
+        global.rcanal
+      )
+    }
+  },
+          }
