a268b8db1cdc473e9626e45cbe24d2ef6f2032fc
S3NJU <s3nju.best.ofc@gmail.com>
2026-05-24 01:53:49 -0300

    Create subbot.js

---
+++ commands/socket/subbot.js
@@ -0,0 +1,30 @@
+import { startSubBot } from '../../columbina/lib/subs.js';
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url'
+
+const filename = fileURLToPath(import.meta.url)
+const dirname = path.dirname(filename)
+
+let commandFlags = {}
+
+export default {
+  command: ['code', 'qr'],
+  category: 'socket',
+  run: async (client, m, args, command) => {
+
+    commandFlags[m.sender] = true
+
+    const rtx = '`✤` Vincula tu *cuenta* usando el *codigo.*\n\n> ✥ Sigue las *instrucciones*\n\n*›* Click en los *3 puntos*\n*›* Toque *dispositivos vinculados*\n*›* Vincular *nuevo dispositivo*\n*›* Selecciona *Vincular con el número de teléfono*\n\nꕤ *`Importante`*\n> ₊·( 🜸 ) ➭ Este *Código* solo funciona en el *número que lo solicito*'
+    const rtx2 = "`✤` Vincula tu *cuenta* usando *codigo qr.*\n\n> ✥ Sigue las *instrucciones*\n\n*›* Click en los *3 puntos*\n*›* Toque *dispositivos vinculados*\n*›* Vincular *nuevo dispositivo*\n*›* Escanea el código *QR.*\n\n> ₊·( 🜸 ) ➭ Recuerda que no es recomendable usar tu cuenta principal para registrar un socket."
+
+    const isCode = /^(code)$/.test(command)
+    const isCommands = /^(code|qr)$/.test(command)
+    const isCommand = isCommands ? true : false
+    const caption = isCode ? rtx : rtx2
+    const phone = args[0] ? args[0].replace(/\D/g, '') : m.sender.split('@')[0]
+
+   // m.reply(phone)
+    await startSubBot(m, client, caption, isCode, phone, m.chat, commandFlags, isCommand)
+  }
+};
